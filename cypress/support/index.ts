82329a4cdcfb3510d33487faf4ab930b77e1f8a7
Anandteerth Onkar <mailanandteerth@gmail.com>
2021-05-04 01:29:11 +0100

    update global.d.ts

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,15 +1,14 @@
-
 // Import commands.js using ES2015 syntax:
-import './commands';
+import "./commands";
 import addContext from "mochawesome/addContext";
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-import 'cypress-xpath';
-import 'cypress-plugin-tab';
+import "cypress-xpath";
+import "cypress-plugin-tab";
 
 Cypress.on("test:after:run", (test, runnable) => {
-    if (test.state === "failed") {
-      const screenshot = `assets/${Cypress.spec.name}/${runnable.parent!.title} - ${test.title} (failed).png`;
-      addContext({ test }, screenshot);
-    }
-  });
+  if (test.state === "failed") {
+    const screenshot = `assets/${Cypress.spec.name}/${runnable.parent!.title} - ${test.title} (failed).png`;
+    addContext({ test }, screenshot);
+  }
+});
